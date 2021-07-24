3b687b9723af161c3eea13283e5afbda48470e0c
Steinar <56594043+SteinarSi@users.noreply.github.com>
2021-07-25 00:19:25 +0200

    Begynte på et nytt interface for backenden: Gameboard.

---
+++ src/backend/gameboard.rs
@@ -0,0 +1,39 @@
+use crate::backend::board_representation::board::*;
+use crate::backend::ai::interface::AI;
+use crate::backend::ai::omikron::Omikron;
+
+pub struct Gameboard{
+	board: Board,
+	ai: Box<AI>
+}
+
+impl Gameboard{
+	pub fn new() -> Self{
+		Gameboard{ai: Box::new(Omikron::new()), board: Board::new()}
+	}
+
+	pub fn move_piece(&mut self, s: &str) -> Option<()>{
+		self.board.move_str(s)
+	}
+
+	pub fn moves(&mut self) -> Moves{
+		self.board.moves()
+	}
+
+	pub fn start_bot(&mut self){
+		let m = self.ai.search(self.board.clone());
+		self.board.move_piece(&m);
+	}
+
+	pub fn is_checkmate(&mut self) -> bool{
+		self.board.is_checkmate()
+	}
+
+	pub fn winner(&mut self) -> Option<Color>{
+		if self.board.is_checkmate() || self.board.is_draw_by_repetition(){
+			self.board.winner()
+		}else{
+			panic!("Why would you ask for a winner when the game is still ongoing!");
+		}
+	}
+}
--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,2 +1,4 @@
-pub mod ai;
-pub mod board_representation;
+mod ai;
+mod board_representation;
+pub mod gameboard;
+pub mod bot_testing;

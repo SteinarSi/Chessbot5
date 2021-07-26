69f162ac038db7f3822498a8f1503ac01edbc507
SteinarSi <steinar.simonnes@gmail.com>
2021-07-26 14:04:15 +0200

    Lagde en ny og mye bedre åpningsdatabase.

--- src/backend/bot_testing.rs
+++ src/backend/bot_testing.rs
@@ -6,10 +6,10 @@ use std::io;
 
 pub fn test_bot(){
     //vs(&mut MemoMax::new(), 5, &mut Quiescence::new(), 8);
-    //simulate(&mut IDDFS::new(), 99);
+    simulate(&mut Omikron::new(), 99);
     //play_against(&mut IDDFS::new(), 99, White);
     //play_against(&mut Omikron::new(), 99, White);
-    vs(&mut Omikron::new(), 9, &mut MemoMax::new(), 4);
+    //vs(&mut Omikron::new(), 9, &mut MemoMax::new(), 4);
     //compare(&mut [("Quiescence", &mut Quiescence::new()), ("PVS", &mut PVS::new())], 10, 8);
 }
 
